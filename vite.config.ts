d0260a044f60c90e9412d87b2b83a0a1d1005e27
azgiliat <toxa2088@gmail.com>
2023-03-13 14:34:30 +0300

    fix lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
       { find: 'assets', replacement: '/src/assets' },
     ],
   },
-})
+});
